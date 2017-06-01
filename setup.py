003fcd6bd54666c4871da8d4e370420a7dbf96d1
Ronan Klyne <ronan.klyne@virtualstock.co.uk>
2017-06-01 15:34:29 +0100

    v0.2.0 - supports post body checking

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='mockwebserver',
-    version='0.1.0',
+    version='0.2.0',
     description='A simple web server for unit testing purposes. Acts as context manager for teardown.',
     author='Virtualstock',
     author_email='dev.admin@virtualstock.com',
@@ -12,5 +12,5 @@ setup(
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     include_package_data=True,
-    install_requires=[],
+    install_requires=['attrs'],
 )
